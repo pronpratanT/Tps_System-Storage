5fd2dd85d2519e147c815c85bbab057467d35f6c
pronpratanT <pronpratant@gmail.com>
2024-07-12 02:48:47 +0700

    fixed 2 test deploy

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,47 +1,54 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { connectMongoDB } from "../../../../../lib/mongodb"; // Adjust the import path as necessary
-import User from "../../../../../models/user"; // Adjust the import path as necessary
+import { connectMongoDB } from "../../../../../lib/mongodb";
+import User from "../../../../../models/user";
 import bcrypt from 'bcryptjs';
 
 const authOptions = {
   providers: [
-      CredentialsProvider({
-        name: 'credentials',
-        credentials: {
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {},
+      async authorize(credentials, req) {
+        const { email, password } = credentials;
+        try {
+          await connectMongoDB();
+          console.log("Connected to MongoDB");
+          
+          const user = await User.findOne({ email });
+          console.log("User found:", user ? "Yes" : "No");
 
-        },
-        async authorize(credentials, req) {
-          const {email, password} = credentials;
-          try{
-              await connectMongoDB();
-              const user = await User.findOne({email});
-
-              if(!user){
-                  return null;
-              }
-              const passwordMatch = await bcrypt.compare(password, user.password);
-
-              if(!passwordMatch){
-                  return null;
-              }
-              return user;
-
-          }catch(error){
-              console.log("Error", error);
+          if (!user) {
+            console.log("User not found");
+            return null;
           }
 
+          const passwordMatch = await bcrypt.compare(password, user.password);
+          console.log("Password match:", passwordMatch);
+
+          if (!passwordMatch) {
+            console.log("Password does not match");
+            return null;
+          }
+
+          console.log("Login successful");
+          return user;
+        } catch (error) {
+          console.error("Error in authorize function:", error);
+          throw new Error("Authentication failed");
         }
-      })
-    ],
-    session: {
-      strategy: "jwt",
-    },
-    secret: process.env.NEXTAUTH_SECRET,
-    pages: {
-      signIn: "/login"
-    },
+      }
+    })
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/login"
+  },
+  debug: process.env.NODE_ENV === 'development',
 }
 
 const handler = NextAuth(authOptions);
-export{handler as GET, handler as POST};
+export { handler as GET, handler as POST };
